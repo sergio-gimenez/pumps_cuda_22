9a822814778197de407f94059fb8a36fe5ffe460
Sergio Giménez <sergiogimenezanton@gmail.com>
2022-09-04 14:48:05 +0200

    .gitignore is now working

--- gather/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nct01/nct01051/pumps_cuda_22/gather
-# Build directory: /home/nct01/nct01051/pumps_cuda_22/gather/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
